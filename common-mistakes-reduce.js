72883a5fe4e59cbeb111e91f2b060cc898008e60
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-21 12:25:44 -0500

    Common mistakes

---
+++ common-mistakes-reduce.js
@@ -0,0 +1,6 @@
+// Reducer's have a default value for inital value, 
+// the first item in the array, 
+// IS the initial value.
+//
+// MUST PASS IN AN INITIAL VALUE
+// MUST RETURN THE ACCUMULATOR
